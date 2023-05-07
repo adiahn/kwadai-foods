e1b60e587fc5c1407db3830a08591c3ffac68e4a
Adiahnn <adnanmukhtar2321@gmail.com>
2023-05-07 18:18:13 +0100

    Added the card about

--- src/components/Home/ProductsList/CardModal.js
+++ src/components/Home/ProductsList/CardModal.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Grade } from '@mui/icons-material';
 import './CardModal.css';
 import garri from '../../../Products/garri.jpg';
 
-
-const CardModal = ({onCloseModal}) => {
+const CardModal = ({ onCloseModal, product }) => {
+  const { title, price } = product;
+  const [isAdded, setIsAdded] = useState(false);
+  const handleAddToCart = () => {
+    setIsAdded(true)
+  }
+  
   return (
     <div className='card-modal-container'>
-
       <div className="card-modal">
         <div className="image-holder">
           <img src={garri} alt='Product Image' />
         </div>
         <div className="product-description">
-          <h1>Garri</h1>
+          <h1>{title}</h1>
           <p>Lorem ipsum dolor sit amet consectetur lobit conse tutes rotan</p>
-          <Link to="#">Kwadai Foods</Link>
+          <Link to="#">{price}</Link>
           <div className="ratings">
             <Grade className='Grade' />
             <Grade className='Grade' />
@@ -25,8 +29,11 @@ const CardModal = ({onCloseModal}) => {
             <Grade className='Grade' />
           </div>
           <div className="card-buttons">
-            <button>To cart</button>
-            <button>Buy now</button>
+            <button onClick={handleAddToCart}>
+              {isAdded ? 'Added' : 'To Cart'}
+            </button>
+            <button>
+              Buy now</button>
             <button onClick={onCloseModal}>Cancel</button>
           </div>
         </div>
--- src/components/Home/ProductsList/Products.js
+++ src/components/Home/ProductsList/Products.js
@@ -5,26 +5,26 @@ import CardModal from './CardModal';
 
 const Products = () => {
   const MyProducts = [
-    { id: 1, title: 'Garri', price: 10.99},
-    { id: 2, title: 'Pounded Yam', price: 15.99},
-    { id: 3, title: 'Egusi Soup', price: 12.99},
-    { id: 4, title: 'Jollof Rice', price: 9.99},
-    { id: 5, title: 'Chicken', price: 8.99},
-    { id: 6, title: 'Beef Stew', price: 13.99},
-    { id: 7, title: 'Fried Rice', price: 11.99},
-    { id: 8, title: 'Fufu', price: 16.99},
-    { id: 9, title: 'Pounded Yam and Egusi Soup', price: 17.99},
-    { id: 10, title: 'Beans and Plantain', price: 7.99},
-    { id: 11, title: 'Suya', price: 6.99},
-    { id: 12, title: 'Efo Riro', price: 14.99},
-    { id: 13, title: 'Akara', price: 5.99},
-    { id: 14, title: 'Moi Moi', price: 4.99},
-    { id: 15, title: 'Fried Chicken', price: 9.99},
-    { id: 16, title: 'Okra Soup', price: 12.99},
-    { id: 17, title: 'Amala', price: 14.99},
-    { id: 18, title: 'Banga Soup', price: 15.99},
-    { id: 19, title: 'Edikang Ikong Soup', price: 16.99},
-    { id: 20, title: 'Fisherman Soup', price: 19.99}
+    { id: 1, title: 'Garri', price: 10.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 2, title: 'Pounded Yam', price: 15.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 3, title: 'Egusi Soup', price: 12.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 4, title: 'Jollof Rice', price: 9.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 5, title: 'Chicken', price: 8.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 6, title: 'Beef Stew', price: 13.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 7, title: 'Fried Rice', price: 11.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 8, title: 'Fufu', price: 16.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 9, title: 'Pounded Yam and Egusi Soup', price: 17.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 10, title: 'Beans and Plantain', price: 7.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 11, title: 'Suya', price: 6.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 12, title: 'Efo Riro', price: 14.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 13, title: 'Akara', price: 5.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 14, title: 'Moi Moi', price: 4.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 15, title: 'Fried Chicken', price: 9.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 16, title: 'Okra Soup', price: 12.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 17, title: 'Amala', price: 14.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 18, title: 'Banga Soup', price: 15.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 19, title: 'Edikang Ikong Soup', price: 16.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'},
+    { id: 20, title: 'Fisherman Soup', price: 19.99, ProductAbout: 'Lorem Ipsum Dolor sit amet consectetur'}
   ];
   
   const [selectedProduct, setSelectedProduct] = useState(null);
@@ -42,7 +42,6 @@ const Products = () => {
       ))}
       {selectedProduct && (
         <CardModal
-
           product={selectedProduct}
           onCloseModal={handleCloseModal}
         />
